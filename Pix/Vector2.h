13f3c3aff7f4955af94e22c80c670af8d8b2ca27
S4gaciter <109637447+S4gaciter@users.noreply.github.com>
2023-01-05 21:17:09 -0800

    Minor Tweaks

--- Pix/Vector2.h
+++ Pix/Vector2.h
@@ -2,30 +2,33 @@
 
 struct Vector2
 {
-	float x;
-	float y;
+	float x, y;
+
+	Vector2() { x = 0; y = 0; }
+	Vector2(float x = 0, float y = 0) { Vector2::x = x; Vector2::y = y; }
 
 	Vector2 operator+ (Vector2 const& obj)
 	{
 		x = x + obj.x;
 		y = y + obj.y;
+		return Vector2(x, y);
 	}
-
 	Vector2 operator- (Vector2 const& obj)
 	{
 		x = x - obj.x;
 		y = y - obj.y;
-	}
-	
+		return Vector2(x, y);
+	}	
 	Vector2 operator* (Vector2 const& obj)
 	{
 		x = x * obj.x;
 		y = y * obj.y;
+		return Vector2(x, y);
 	}
-
 	Vector2 operator/ (Vector2 const& obj)
 	{
 		x = x / obj.x;
 		y = y / obj.y;
+		return Vector2(x, y);
 	}
 };
--- Pix/Vector3.h
+++ Pix/Vector3.h
@@ -2,35 +2,37 @@
 
 struct Vector3
 {
-	float x = 0;
-	float y = 0;
-	float z = 0;
+	float x, y, z;
+
+	Vector3() { x = 0; y = 0; z = 0; }
+	Vector3(float x = 0, float y = 0, float z = 0) { Vector3::x = x; Vector3::y = y; Vector3::z = z; }
 
 	Vector3 operator+ (Vector3 const& obj)
 	{
 		x = x + obj.x;
 		y = y + obj.y;
 		z = z + obj.z;
+		return Vector3(x, y, z);
 	}
-
 	Vector3 operator- (Vector3 const& obj)
 	{
 		x = x - obj.x;
 		y = y - obj.y;
 		z = z - obj.z;
+		return Vector3(x, y, z);
 	}
-
 	Vector3 operator* (Vector3 const& obj)
 	{
 		x = x * obj.x;
 		y = y * obj.y;
 		z = z * obj.z;
+		return Vector3(x, y, z);
 	}
-
 	Vector3 operator/ (Vector3 const& obj)
 	{
 		x = x / obj.x;
 		y = y / obj.y;
 		z = z / obj.z;
+		return Vector3(x, y, z);
 	}
 };
